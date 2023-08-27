961f963cb1609c027cc2a34f379880edbf7c9194
Achala-Nayak <106529093+Achala-Nayak@users.noreply.github.com>
2023-08-28 01:23:01 +0530

    Updated-Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,8 +24,8 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Achala &nbsp;
-            <span className='sm:block hidden'> | JavaScript Mastery</span>
+            Achala Nayak &nbsp;
+            <span className='sm:block hidden'> </span>
           </p>
         </Link>
       <ul className='list-none hidden sm:flex flex-row gap-10'>
